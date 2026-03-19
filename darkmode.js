e8072d1b413e1ee50ff61d8021405d5c26ca060f
oompatom <147835159+oompatom@users.noreply.github.com>
2026-03-19 23:38:03 +0700

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,11 @@
+let modeBtn = document.getElementById("darkLight");
+
+modeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        modeBtn.innerText = "Light Mode";
+    } else {
+        modeBtn.innerText = "Dark Mode";
+    }
+});
